25e37a87fc25bc8d3009bab69193e710d18717a1
codelahiru <gayan.hatharasinghe@gmail.com>
2024-06-16 22:56:53 +0530

    train and test split

--- keras-diabetes-dataset/main.py
+++ keras-diabetes-dataset/main.py
@@ -4,30 +4,34 @@ from tensorflow.keras.layers import Dense
 
 # Load dataset
 dataset = np.loadtxt('data.csv', delimiter=',')
-# Split into input (X) and output (Y) variables
-X = dataset[:, 0:8]
-Y = dataset[:, 8]
+# Split data into train input (X) and train output (Y) / test input and output variables
+
+train_X = dataset[0:615, 0:8]
+train_Y = dataset[0:615, 8]
+test_X = dataset[615:768, 0:8]
+test_Y = dataset[615:768, 8]
 
 # Define the Keras model
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(1, input_dim=8, activation='relu'))
+model.add(Dense(1, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+model.summary()
 
 # Compile the Keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the Keras model on the dataset
-model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
+model.fit(train_X, train_Y, epochs=150, batch_size=10, verbose=0)
 
 # Evaluate the Keras model
-_, accuracy = model.evaluate(X, Y)
+_, accuracy = model.evaluate(train_X, train_Y)
 print('Accuracy: %.2f' % (accuracy * 100))
 
 #predictions = myANN.predict_classes(data_input)
-predictions = (model.predict(X) > 0.5).astype(int)
+predictions = (model.predict(test_X) > 0.9).astype(int)
 
 for i in range(20):
-  print(Y[i], " ", predictions[i])
+  print(test_Y[i], " ", predictions[i])
 
 
